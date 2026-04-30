0415a59ba468a0cf3da428e9ba4852519c694668
wakutaipa <1032239009@rudn.ru>
2026-04-30 15:55:20 +0300

    feat(main): added lab 06 files

---
+++ labs/lab06/Project/scripts/sirpetri_run.jl
@@ -0,0 +1,32 @@
+using DrWatson
+@quickactivate "Project"
+using Random
+include(srcdir("SIRPetri.jl"))
+using .SIRPetri
+using DataFrames, CSV, Plots
+
+# Параметры
+β = 0.3
+γ = 0.1
+tmax = 100.0
+
+# Создаём сеть
+net, u0, states = build_sir_network(β, γ)
+
+# Детерминированная симуляция
+df_det = simulate_deterministic(net, u0, (0.0, tmax), saveat = 0.5, rates = [β, γ])
+CSV.write(datadir("sir_det.csv"), df_det)
+
+# Стохастическая симуляция
+Random.seed!(123)
+df_stoch = simulate_stochastic(net, u0, (0.0, tmax), rates = [β, γ])
+CSV.write(datadir("sir_stoch.csv"), df_stoch)
+
+# Графики
+p_det = plot_sir(df_det)
+savefig(plotsdir("sir_det_dynamics.png"))
+
+p_stoch = plot_sir(df_stoch)
+savefig(plotsdir("sir_stoch_dynamics.png"))
+
+println("Базовый прогон завершён. Результаты в data/ и plots/")
